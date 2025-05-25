c83964cc8d789fab6ecf22b2ab8e74646b51e3b5
Driannn <drianbetancourt@gmail.com>
2025-05-25 12:20:37 -0300

    Main menu, background en el juego, audio manager setup

---
+++ src/audio_manager.gd
@@ -0,0 +1,12 @@
+extends Node
+
+#SFX
+@export_group("SFX")
+@export var eat: AudioStreamPlayer
+@export var positive_reaction: AudioStreamPlayer
+@export var negative_reaction: AudioStreamPlayer
+@export var nice_combo: AudioStreamPlayer
+
+#MX
+@export_group("Music")
+@export var bg_music: AudioStreamPlayer
